6df882c7e38f2f119ddf274a504cdd00ecac6f96
mushfikurahmaan <mushfikurahmaan@gmail.com>
2025-04-21 09:53:00 +0600

    Fix tailwind configuration error - remove inline config and update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-module.exports = {content: ['./templates/**/*.html', './static/**/*.js'], theme: {extend: {colors: {primary: '#A15DF8', black: '#000000',}, fontFamily: {'satoshi': ['Satoshi', 'sans-serif'],}}}, plugins: [],}
+module.exports = {
+  content: ['./templates/**/*.html', './static/**/*.js'], 
+  theme: {
+    extend: {
+      colors: {
+        primary: '#A15DF8',
+        black: '#000000',
+      },
+      fontFamily: {
+        'satoshi': ['Satoshi', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif'],
+      }
+    }
+  }, 
+  plugins: [],
+}
